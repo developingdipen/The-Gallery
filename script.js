96645fd8777db9f6707a31cb8b1e588158bd8cfa
Dipen Patel <60754812+developingdipen@users.noreply.github.com>
2020-05-08 20:28:47 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
